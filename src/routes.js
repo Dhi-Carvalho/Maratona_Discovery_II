31826957c0d960a0a1e3a04b9d4614c72de44dd1
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-04-08 16:02:09 -0300

    Creation of the Update Profile function that calculates the average hourly rate

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,24 @@ const Profile = {
     index(req, res) {
       return res.render(views + "profile", { profile: Profile.data });
     },
+
+    update(req, res) {
+      const data = req.body;
+      const weeksPerYear = 52;
+      const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12;
+      const weekTotalHours = data["hours-per-day"] * data["days-per-week"];
+      const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+
+      const valueHour = data["monthly-budget"] / monthlyTotalHours;
+
+      Profile.data = {
+        ...Profile.data,
+        ...req.body,
+        "value-hour": valueHour,
+      };
+
+      return res.redirect("/profile");
+    },
   },
 };
 
@@ -100,5 +118,7 @@ routes.post("/job", Job.controllers.save);
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
 
 routes.get("/profile", Profile.controllers.index);
+
+routes.post("/profile", Profile.controllers.update);
 
 module.exports = routes;
