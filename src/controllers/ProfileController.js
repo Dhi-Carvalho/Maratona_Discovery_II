4dd594f56ee544f4572ee77be017c305ed14411e
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-04-13 10:08:24 -0300

    Added await function in  update

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -16,7 +16,7 @@ module.exports = {
 
     const profile = await Profile.get()
 
-    Profile.update({
+    await Profile.update({
       ...profile,
       ...req.body,
       "value-hour": valueHour,
