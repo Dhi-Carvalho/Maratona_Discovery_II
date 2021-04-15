793f1f6705918e0fde0f81cd1804541740129885
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-04-15 11:43:06 -0300

    Console.log removal

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -54,8 +54,6 @@ module.exports = {
 
     await Job.delete(jobId);
 
-    console.log(jobId)
-
     return res.redirect("/");
   },
 };
--- src/server.js
+++ src/server.js
@@ -10,4 +10,4 @@ server.use(express.static("public"));
 server.use(express.urlencoded({ extended: true }))
 server.use(routes);
 
-server.listen(3000, () => console.log("rodando"));
+server.listen(3000);
